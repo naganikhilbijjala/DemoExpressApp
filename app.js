055606b0451ee752dcd5b70e86365dcdb4a79fec
Naga Nikhil Bijjala <naganikhil.bijjala@gmail.com>
2023-02-14 17:25:15 -0500

    add routes

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const app = express();
 let port = 3000;
 let host = 'localhost';
 
+app.get('/', (req, res) => {
+    // res.statusCode = 200;
+    // res.end('Home page');
+    // Instead of doing like above there is a simple way in express
+    res.send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Page');
+});
+
 app.listen(port, host, () => {
-    console.log('The server is running on port', port);
+    console.log('The server is running at port', port);
 });

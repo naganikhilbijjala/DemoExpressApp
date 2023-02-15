7112f37da09ca62c5ebadf6577c57dffed1a675c
Naga Nikhil Bijjala <naganikhil.bijjala@gmail.com>
2023-02-14 20:07:11 -0500

    req and res object access

--- app.js
+++ app.js
@@ -4,11 +4,35 @@ const app = express();
 let port = 3000;
 let host = 'localhost';
 
+let students = [{id: 1, name: "Nikhil"}, {id: 2, name: "Nithin"}, {id: 3, name:"Adithya"}];
+
 app.get('/', (req, res) => {
     // res.statusCode = 200;
     // res.end('Home page');
     // Instead of doing like above there is a simple way in express
-    res.send('Home Page');
+    // res.send('Home Page');
+    // console.log(__dirname);
+    // dirname gives the abosolute path to the directory where the app.js is located
+    console.log(req.url);
+    console.log(req.query);
+
+    res.sendFile('./views/index.html',{root: __dirname});
+});
+
+// Send student with particular id back to the client
+app.get('/students/:sid', (req, res) => {
+    console.log(req.params);
+    let sid = req.params.sid;
+    let student = students.find(element => element.id == parseInt(sid));
+    console.log(student);
+    res.json(student);
+    // sends empty body when student object is undefined
+});
+
+
+// Redirect
+app.get('/contact-us', (req, res) => {
+    res.redirect('/contact');
 });
 
 app.get('/about', (req, res) => {
